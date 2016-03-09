8d0ee0723528877ab69e7982fcb46247255300c4
Kenneth Söderlund <kenneth.soderlund@nordsoftware.com>
2016-03-09 11:45:27 +0200

    Add template files.

---
+++ templates/admin/meta-box.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * WooCommerce Maksuturva Payment Gateway
+ *
+ * @package WooCommerce Maksuturva Payment Gateway
+ */
+
+/**
+ * Maksuturva Payment Gateway Plugin for WooCommerce 2.x
+ * Plugin developed for Maksuturva
+ * Last update: 08/03/2016
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * [GNU LGPL v. 2.1 @gnu.org] (https://www.gnu.org/licenses/lgpl-2.1.html)
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Variables defined.
+ *
+ * @var string $message The message to print out.
+ *
+ * @since 2.0.0
+ */
+?>
+
+<p class="comment-notes">
+	<?php echo esc_attr( $message ); ?>
+</p>
+
