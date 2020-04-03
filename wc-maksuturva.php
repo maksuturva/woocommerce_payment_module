f1a0ff1e0c23bd4288217e207a3f81845eff0546
Henri Behm <henri.behm@svea.fi>
2020-04-03 11:38:44 +0300

    Version number change to 2.1.2

--- wc-maksuturva.php
+++ wc-maksuturva.php
@@ -8,9 +8,9 @@
  * Plugin Name:  Svea Payment Gateway
  * Plugin URI:   https://github.com/maksuturva/woocommerce_payment_module
  * Description: A plugin for Svea Payments, which provides intelligent online payment services consisting of the most comprehensive set of high quality service features in the Finnish market
- * Version:     2.1.1
+ * Version:     2.1.2
  * Author:      Svea Development Oy
- * Author URI:  http://www.maksuturva.fi
+ * Author URI:  http://www.sveapayments.fi
  * Text Domain: wc-maksuturva
  * Domain Path: /languages/
  * Requires at least: 4.0
@@ -76,7 +76,7 @@ class WC_Maksuturva {
 	 *
 	 * @var string VERSION The plugin version.
 	 */
-	const VERSION = '2.1.1';
+	const VERSION = '2.1.2';
 
 	/**
 	 * Plugin DB version.
