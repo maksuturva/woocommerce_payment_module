5eafc3092d4ffc9cfee485ec9a33851af86cca97
Svea Maintainer <henri.behm-ext@svea.fi>
2022-02-07 18:54:33 +0200

    Extra check to status query

--- includes/class-wc-payment-checker-maksuturva.php
+++ includes/class-wc-payment-checker-maksuturva.php
@@ -214,8 +214,8 @@ class WC_Payment_Checker_Maksuturva {
 		$now_time = strtotime(date('Y-m-d H:i:s'));
 
 		$create_diff = $now_time - strtotime($payment_date_added);
-		/* if there is no 'updated date', so do status query */
-		if (is_null($payment_date_updated)) {
+		/* if there is no 'updated date', so do status query if order is created max 7 days ago */
+		if (is_null($payment_date_updated) && $this->in_range($create_diff, 0, 168*3600)) {
 			// _log("DEBUG No 'updated date'. Is_time_to_check " .  $payment_date_added . " is true.");
 			return true;
 		}
