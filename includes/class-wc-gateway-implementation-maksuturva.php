f7e207454bd3dcd6d59a2a35f0ad95bd4535a7bf
Svea Maintainer <henri.behm-ext@svea.fi>
2022-09-08 21:11:15 +0300

    Fixed double fee problem when using payment fee

--- includes/class-wc-gateway-implementation-maksuturva.php
+++ includes/class-wc-gateway-implementation-maksuturva.php
@@ -255,13 +255,11 @@ class WC_Gateway_Implementation_Maksuturva extends WC_Gateway_Abstract_Maksuturv
 
 		$payment_row_handling_cost = $this->create_payment_row_handling_cost_data( $payment_method_handling_cost );
 		if ( is_array( $payment_row_handling_cost ) ) {
-			error_log("HENE HANDLING COST LÖYTYI" . print_r( $payment_row_handling_cost, true), 0);
 			$payment_rows[] = $payment_row_handling_cost;
 		}
 
 		$payment_row_fees = $this->create_payment_row_fee_data( $order );
 		if ( is_array( $payment_row_fees ) ) {
-			error_log("HENE PAYMENT FEE LÖYTYI" . print_r($payment_row_fees ,true), 0);
 			$payment_rows = array_merge( $payment_rows, $payment_row_fees );
 		}
 
@@ -359,7 +357,6 @@ class WC_Gateway_Implementation_Maksuturva extends WC_Gateway_Abstract_Maksuturv
 
 		$payment_handling_costs_handler = new WC_Payment_Handling_Costs( $this->wc_gateway );
 		$tax_rate = $payment_handling_costs_handler->get_payment_method_handling_cost_tax_rate();
-		error_log("### HENEFEE PHF " . print_r($payment_method_handling_cost, true), 0 );
 
 		return [
 			'pmt_row_name'               => __( 'Payment handling fee', $this->wc_gateway->td ),
@@ -392,18 +389,13 @@ class WC_Gateway_Implementation_Maksuturva extends WC_Gateway_Abstract_Maksuturv
 
 			$fee_total = $fee['line_total'] + $fee['line_tax'];
 
-			error_log("HENE FEE NAME: '" . $fee['name'] . "'");
-			error_log("HENE FEE NAME2: '" . WC_Utils_Maksuturva::filter_description( $fee['name'] ) . "'");
-
 			if (WC_Utils_Maksuturva::filter_description( $fee['name'] ) === "-") {
 				// don't add fee with empty trimmed description (double fee problem)
 				$this->removed_fees += $fee_total;
-				error_log("### HENE SKIP EMPTY FEE");
 				continue;
 			}
 
 			if (WC_Utils_Maksuturva::filter_description( $fee['name'] ) === __( 'Payment handling fee', $this->wc_gateway->td )) {
-				error_log("### HENEFEE Match Payment handling fee", 0);
 				$this->removed_fees += $fee_total;
 				continue;
 			}
--- wc-maksuturva.php
+++ wc-maksuturva.php
@@ -8,7 +8,7 @@
  * Plugin Name:  Svea Payment Gateway
  * Plugin URI:   https://github.com/maksuturva/woocommerce_payment_module
  * Description: A plugin for Svea Payments, which provides intelligent online payment services consisting of the most comprehensive set of high quality service features in the Finnish market
- * Version:     2.2.1  
+ * Version:     2.2.2    
  * Author:      Svea Development Oy
  * Author URI:  http://www.sveapayments.fi
  * Text Domain: wc-maksuturva
@@ -23,7 +23,7 @@
 /**
  * Svea Payments Gateway Plugin for WooCommerce 5.x, 6.x
  * Plugin developed for Svea Development Oy
- * Last update: 15/08/2022
+ * Last update: 8/9/2022
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -76,7 +76,7 @@ class WC_Maksuturva {
 	 *
 	 * @var string VERSION The plugin version.
 	 */
-	const VERSION = '2.2.1';
+	const VERSION = '2.2.2';
 
 	/**
 	 * Plugin DB version.
