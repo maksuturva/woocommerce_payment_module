0f597d9ad74830f19675c62a650a1d9de85052dd
Henri Behm <henri.behm-ext@svea.fi>
2023-04-10 21:25:31 +0300

    Collated template

--- includes/class-wc-gateway-svea-collated.php
+++ includes/class-wc-gateway-svea-collated.php
@@ -8,7 +8,7 @@
 /**
  * Svea Payments Gateway Plugin for WooCommerce 6.x, 7.x
  * Plugin developed for Svea
- * Last update: 3/4/2020
+ * Last update: 6/4/2023
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -27,32 +27,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Class WC_Gateway_Svea_Estonia_Payments
+ * Class WC_Gateway_Svea_Collated
  *
  * Gateway handler for credit card and mobile payments
  *
- * @since 2.1.4
+ * @since 2.1.3
  */
-class WC_Gateway_Svea_Estonia_Payments extends WC_Gateway_Maksuturva {
+class WC_Gateway_Svea_Collated extends WC_Gateway_Maksuturva {
 
 	/**
-	 * WC_Gateway_Svea_Estonia_Payments constructor.
+	 * WC_Gateway_Svea_Collated constructor.
 	 *
 	 * Sets the values for gateway specific parent class properties
 	 *
-	 * @since 2.1.4
+	 * @since 2.3.10
 	 */
 	public function __construct() {
-		parent::__construct( WC_Gateway_Svea_Estonia_Payments::class );
-		$this->method_title = 'Svea ' . __( 'Payments Estonia', $this->td );
-		$this->method_description = sprintf( 
-			__('This payment method is for Estonia partners only. General Svea settings are managed <a href="%s">here</a>.', $this->td), '?page=wc-settings&tab=checkout&section=wc_gateway_maksuturva' );
-		$custom_title = $this->get_option( 'payment_group_estonia_title' );
-		if (!empty($custom_title)) {
-			$this->title = esc_html($custom_title);
-		} else {
-			$this->title = __( 'Ostukonto - maksa turvaliselt ja paindlikult', $this->td );
-		}
+		parent::__construct( WC_Gateway_Svea_Collated::class );
+		$this->method_title = 'Svea ' . __( 'Collated Payments', $this->td );
+		$this->method_description = sprintf( __( 'General Svea settings are managed <a href="%s">here</a>.', $this->td), '?page=wc-settings&tab=checkout&section=wc_gateway_maksuturva' );
+		$this->title = __( 'Collated', $this->td );
 		$this->icon = WC_Maksuturva::get_instance()->get_plugin_url() . 'Empty_logo.png';
 	}
 
@@ -60,6 +54,6 @@ class WC_Gateway_Svea_Estonia_Payments extends WC_Gateway_Maksuturva {
 	 * @inheritdoc
 	 */
 	public function init_form_fields() {
-    $this->form_fields = [];
-  }
+    	$this->form_fields = [];
+  	}
 }
---
+++ templates/frontend/payment-method-collated-form.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * WooCommerce Svea Payments Gateway
+ *
+ * @package WooCommerce Svea Payments Gateway
+ */
+
+/**
+ * Svea Payments Gateway Plugin for WooCommerce 6.x, 7.x
+ * Plugin developed for Svea
+ * Last update: 30/11/2020
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * [GNU LGPL v. 2.1 @gnu.org] (https://www.gnu.org/licenses/lgpl-2.1.html)
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Variables defined.
+ *
+ * @var string $currency_symbol Symbol for inidicating the currency
+ * @var array $payment_method_handling_costs Configured payment method handling costs.
+ * @var string $payment_method_select_id The name to use for payment method select field.
+ * @var array $payment_methods Available payment methods.
+ * @var array $terms Terms.
+ *
+ * @since 2.1.3
+ */
+?>
+
+<div>
+	<?php
+		echo "<p><b>" . $method_group1['title'] . "</b></p><br />";
+		foreach ( $method_group1['methods'] as $payment_method ) { 
+	?>
+		<div class="svea-payment-method-select" style="clear: both;">
+			<label for="<?php echo $payment_method_select_id; ?>-<?php echo $payment_method['code']; ?>">
+				<img
+					alt="<?php echo $payment_method['displayname']; ?>"
+					src="<?php echo $payment_method['imageurl']; ?>"
+				/>
+			</label>
+			<?php 
+				foreach ( $payment_method_handling_costs as $handling_cost ) {
+					if ( $handling_cost['payment_method_type'] === $payment_method['code'] ) {
+						echo '<div class="handling-cost-amount">+' . WC_Utils_Maksuturva::filter_price( $handling_cost['handling_cost_amount'] ) . ' ' . $currency_symbol . '</div>';
+						break;
+					}
+				}
+			?>
+			<input
+				class="input-radio svea-payment-method-select-radio"
+				id="<?php echo $payment_method_select_id; ?>-<?php echo $payment_method['code']; ?>"
+				name="<?php echo $payment_method_select_id; ?>"
+				type="radio"
+				value="<?php echo $payment_method['code']; ?>"
+			/>
+		</div>
+	<?php } ?>
+
+	<?php
+		echo "<p><b>" . $method_group2['title'] . "</b></p><br />";
+		foreach ( $method_group2['methods'] as $payment_method ) { 
+	?>
+		<div class="svea-payment-method-select" style="clear: both;">
+			<label for="<?php echo $payment_method_select_id; ?>-<?php echo $payment_method['code']; ?>">
+				<img
+					alt="<?php echo $payment_method['displayname']; ?>"
+					src="<?php echo $payment_method['imageurl']; ?>"
+				/>
+			</label>
+			<?php 
+				foreach ( $payment_method_handling_costs as $handling_cost ) {
+					if ( $handling_cost['payment_method_type'] === $payment_method['code'] ) {
+						echo '<div class="handling-cost-amount">+' . WC_Utils_Maksuturva::filter_price( $handling_cost['handling_cost_amount'] ) . ' ' . $currency_symbol . '</div>';
+						break;
+					}
+				}
+			?>
+			<input
+				class="input-radio svea-payment-method-select-radio"
+				id="<?php echo $payment_method_select_id; ?>-<?php echo $payment_method['code']; ?>"
+				name="<?php echo $payment_method_select_id; ?>"
+				type="radio"
+				value="<?php echo $payment_method['code']; ?>"
+			/>
+		</div>
+	<?php } ?>
+
+	<?php
+		echo "<p><b>" . $method_group3['title'] . "</b></p><br />";
+		foreach ( $method_group3['methods'] as $payment_method ) { 
+	?>
+		<div class="svea-payment-method-select" style="clear: both;">
+			<label for="<?php echo $payment_method_select_id; ?>-<?php echo $payment_method['code']; ?>">
+				<img
+					alt="<?php echo $payment_method['displayname']; ?>"
+					src="<?php echo $payment_method['imageurl']; ?>"
+				/>
+			</label>
+			<?php 
+				foreach ( $payment_method_handling_costs as $handling_cost ) {
+					if ( $handling_cost['payment_method_type'] === $payment_method['code'] ) {
+						echo '<div class="handling-cost-amount">+' . WC_Utils_Maksuturva::filter_price( $handling_cost['handling_cost_amount'] ) . ' ' . $currency_symbol . '</div>';
+						break;
+					}
+				}
+			?>
+			<input
+				class="input-radio svea-payment-method-select-radio"
+				id="<?php echo $payment_method_select_id; ?>-<?php echo $payment_method['code']; ?>"
+				name="<?php echo $payment_method_select_id; ?>"
+				type="radio"
+				value="<?php echo $payment_method['code']; ?>"
+			/>
+		</div>
+	<?php } ?>
+
+	<?php
+		echo "<p><b>" . $method_group4['title'] . "</b></p><br />";
+		foreach ( $method_group4['methods'] as $payment_method ) { 
+	?>
+		<div class="svea-payment-method-select" style="clear: both;">
+			<label for="<?php echo $payment_method_select_id; ?>-<?php echo $payment_method['code']; ?>">
+				<img
+					alt="<?php echo $payment_method['displayname']; ?>"
+					src="<?php echo $payment_method['imageurl']; ?>"
+				/>
+			</label>
+			<?php 
+				foreach ( $payment_method_handling_costs as $handling_cost ) {
+					if ( $handling_cost['payment_method_type'] === $payment_method['code'] ) {
+						echo '<div class="handling-cost-amount">+' . WC_Utils_Maksuturva::filter_price( $handling_cost['handling_cost_amount'] ) . ' ' . $currency_symbol . '</div>';
+						break;
+					}
+				}
+			?>
+			<input
+				class="input-radio svea-payment-method-select-radio"
+				id="<?php echo $payment_method_select_id; ?>-<?php echo $payment_method['code']; ?>"
+				name="<?php echo $payment_method_select_id; ?>"
+				type="radio"
+				value="<?php echo $payment_method['code']; ?>"
+			/>
+		</div>
+	<?php } ?>
+</div>
+
+<p><?php if (!empty($terms['text']) ) { echo $terms['text']; ?>  
+(<a href="<?php echo $terms['url']; ?>" target="_blank">PDF</a>)</p>
+<?php } ?>
+
+<div style="clear: both;"></div>
