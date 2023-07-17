0ce5b0763fdb7303e2ccd1b431e0139bc8f4fa1a
Henri Behm <henri.behm-ext@svea.fi>
2023-07-17 20:44:02 +0300

    Updated version information

--- includes/class-wc-gateway-abstract-maksuturva.php
+++ includes/class-wc-gateway-abstract-maksuturva.php
@@ -1030,6 +1030,7 @@ abstract class WC_Gateway_Abstract_Maksuturva {
 	 * 
 	 * @since 2.4.2
 	 */
+	/*
 	public function diagnostic_request() {
 		if ( ! function_exists( 'curl_init' ) ) {
 			throw new WC_Gateway_Maksuturva_Exception(
@@ -1071,7 +1072,8 @@ abstract class WC_Gateway_Abstract_Maksuturva {
 		}
 		curl_close( $request );
 	}
-
+	*/
+	
 	/**
 	 * Get fields as an array.
 	 *
