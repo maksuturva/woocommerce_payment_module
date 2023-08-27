f0763318c6c3fe31c5dd29d6a87f8b7eb09fab33
Henri Behm <henri.behm-ext@svea.fi>
2023-08-27 20:55:08 +0300

    Updated version info and pmt_row_desc to be always slash

--- includes/class-wc-gateway-abstract-maksuturva.php
+++ includes/class-wc-gateway-abstract-maksuturva.php
@@ -779,8 +779,11 @@ abstract class WC_Gateway_Abstract_Maksuturva {
 				if ( ! trim( $p['pmt_row_name'] ) ) {
 					$this->payment_data['pmt_rows_data'][ $i ]['pmt_row_name'] = $p['pmt_row_name'];
 				} 
-				$this->payment_data['pmt_rows_data'][ $i ]['pmt_row_desc'] = '-';
 			}
+			if ( array_key_exists( 'pmt_row_desc', $p ) ) {
+				$this->payment_data['pmt_rows_desc'][ $i ]['pmt_row_desc'] = '-';
+			}
+
 			foreach ( $p as $k => $value ) {
 				if ( ( array_key_exists( $k, self::$field_filters ) && in_array( $k, self::$mandatory_data ) )
 				     || array_key_exists( $k, self::$field_filters ) && in_array( $k, self::$row_mandatory_data )
