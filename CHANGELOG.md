bb630548dd2697401a727277ef2a719dea1b4e07
Henri Behm <henri.behm-ext@svea.fi>
2024-12-31 13:41:28 +0200

    Updated version information

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## Changelog
 
+### 2.6.12 - 
+
 ### 2.6.11 - 2024-12-12
 * 285659: Redirect payments with error state back to the checkout page.
 * Added filter `svea_payment_gateway_payment_error_return_url` to change the return URL when payment is returned with error state.
--- wc-maksuturva.php
+++ wc-maksuturva.php
@@ -8,7 +8,7 @@
  * Plugin Name:  Svea Payment Gateway
  * Plugin URI:   https://github.com/maksuturva/woocommerce_payment_module
  * Description: A plugin for Svea Payments, which provides intelligent online payment services consisting of the most comprehensive set of high quality service features in the Finnish market
- * Version:     2.6.11        
+ * Version:     2.6.12        
  * Author:      Svea Development Oy
  * Author URI:  http://www.sveapayments.fi
  * Text Domain: wc-maksuturva
@@ -17,7 +17,7 @@
  * Tested up to: 6.7.1       
  * License:      LGPL2.1
  * WC requires at least: 8.0
- * WC tested up to: 9.4.3         
+ * WC tested up to: 9.5.1           
  */
 
 /**
@@ -103,7 +103,7 @@ class WC_Maksuturva {
 	 *
 	 * @var string VERSION The plugin version.
 	 */
-	const VERSION = '2.6.11';
+	const VERSION = '2.6.12';
 
 	/**
 	 * Plugin DB version.
